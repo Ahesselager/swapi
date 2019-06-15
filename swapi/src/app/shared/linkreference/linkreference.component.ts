bf52a3a276748ef86b671137d8d075e0c96e994d
Anders Hesselager <anhe@netcompany.com>
2019-06-15 17:37:56 +0200

    Added content to starship class

--- swapi/src/app/shared/linkreference/linkreference.component.ts
+++ swapi/src/app/shared/linkreference/linkreference.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Objecttype } from '../enums/Objecttype';
+import { PersonService } from 'src/app/person/person.service';
+import { FilmsService } from 'src/app/film/film.service';
+import { StarshipService } from 'src/app/starship/starship.service';
+import { VehicleService } from 'src/app/vehicles/vehicle.service';
+import { PlanetService } from 'src/app/planet/planet.service';
 
 @Component({
     selector: 'swapi-linkreference',
@@ -6,6 +12,30 @@ import { Component } from '@angular/core';
     styleUrls: ['./linkreference.component.scss']
 })
 
-export class LinkReferenceComponent {
+export class LinkReferenceComponent implements OnInit {
+    @Input()
+    referenceObjectType: Objecttype;
+    @Input()
+    url: string;
 
+    constructor(
+        private filmService: FilmsService,
+        private personService: PersonService,
+        private planetService: PlanetService,
+        private speciesService: SpeciesService,
+        private starshipService: StarshipService,
+        private vehicleService: VehicleService,
+    ) { }
+    ngOnInit(): void {
+        switch (this.referenceObjectType) {
+            case Objecttype.Film:
+                this.filmService.getFromFullURL(this.url).then(
+                    
+                )
+                break;
+        
+            default:
+                break;
+        }
+    }
 }
--- swapi/src/app/starship/starship.ts
+++ swapi/src/app/starship/starship.ts
@@ -1,5 +1,22 @@
-export class Starship {
+import { INameable } from '../shared/interfaces/Inameable';
 
+export class Starship implements INameable {
+    name: string;
+    model: string;
+    manufacturer: string;
+    costInCredits: number;
+    length: number;
+    // Is string to accomodate N/A values
+    maxAtmospheringSpeed: string;
+    crew: number;
+    passengers: number;
+    cargoCapacity: number;
+    consumables: string;
+    hyperdriveEating: number;
+    MGLT: number;
+    starshipClass: string;
+    pilots: string[];
+    films: string[];
 
     constructor(planet?: any) {
         if (planet != null) {
