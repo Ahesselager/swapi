a6c096098f5347cae7cc337803533adc7ac30f7c
Anders Hesselager <anhe@netcompany.com>
2019-06-19 19:20:18 +0200

    Added user migration

---
+++ Swapi_Backend/Swapi_Backend/Database/Migrations/201906191719375_user_entity_added.cs
@@ -0,0 +1,26 @@
+namespace Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class user_entity_added : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserName = c.String(nullable: false, maxLength: 128),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.UserName);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
--- Swapi_Backend/Swapi_Backend/Database/User.cs
+++ Swapi_Backend/Swapi_Backend/Database/User.cs
@@ -1,7 +1,10 @@
-﻿namespace Database
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database
 {
     public class User
     {
+        [Key]
         public string UserName { get; set; }
         public string Password { get; set; }
     }
