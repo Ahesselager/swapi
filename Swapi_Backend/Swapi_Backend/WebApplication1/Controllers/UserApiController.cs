a2c1d2630b861c831e03a9bbdbc968e0d6129fb0
Anders Hesselager <anhe@netcompany.com>
2019-06-19 19:49:26 +0200

    added http get to login call

--- Swapi_Backend/Swapi_Backend/WebApplication1/Controllers/UserApiController.cs
+++ Swapi_Backend/Swapi_Backend/WebApplication1/Controllers/UserApiController.cs
@@ -8,6 +8,7 @@ namespace Swapi.Web.Controllers
     public class UserApiController : ApiController
     {
         [Route("login/")]
+        [HttpGet]
         public IHttpActionResult LogIn(string username, string password)
         {
             // This causes a reference to the EF project. This is ugly and should use a repository and a DTO instead. But that is gold plating right now
