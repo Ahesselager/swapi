f474fc6b5cb2243ac0ead3d118696664e2802ef2
Anders Hesselager <anhe@netcompany.com>
2019-06-19 20:01:48 +0200

    made service to pull out EF reference from frontend

--- Swapi_Backend/Swapi_Backend/WebApplication1/Controllers/UserApiController.cs
+++ Swapi_Backend/Swapi_Backend/WebApplication1/Controllers/UserApiController.cs
@@ -1,5 +1,4 @@
 ﻿using Database;
-using System.Linq;
 using System.Web.Http;
 
 namespace Swapi.Web.Controllers
@@ -12,18 +11,9 @@ namespace Swapi.Web.Controllers
         public IHttpActionResult LogIn(string username, string password)
         {
             // This causes a reference to the EF project. This is ugly and should use a repository and a DTO instead. But that is gold plating right now
-            using (var context = new SwapiDbContext())
-            {
-                var foo = context.Users.FirstOrDefault(u=> u.UserName== username && u.Password == password);
-                if (foo != null)
-                {
-                    return Ok(true);
-                }
-                else
-                {
-                    return Ok(false);
-                }
-            }
+
+            var userservice = new Userservice();
+            return Ok(userservice.Login(username, password));
         }
     }
 }
